10dd3bb076575aa6dc5c593bde9f9b39b4889f77
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-06-27 22:20:54 +0100

    Add src/main.py

---
+++ src/main.py
@@ -0,0 +1,50 @@
+"""Module main.py"""
+import datetime
+import logging
+import os
+import sys
+
+import boto3
+
+
+def main():
+    """
+
+    :return:
+    """
+
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
+
+    logger.info(s3_parameters)
+    logger.info(arguments)
+    logger.info(hyperspace)
+
+
+if __name__ == '__main__':
+
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Classes
+    import src.elements.arguments as ag
+    import src.elements.hyperspace as hp
+    import src.elements.s3_parameters as s3p
+    import src.elements.service as sr
+    import src.functions.cache
+    import src.preface.interface
+
+    connector: boto3.session.Session
+    s3_parameters: s3p.S3Parameters
+    service: sr.Service
+    arguments: ag.Arguments
+    hyperspace: hp.Hyperspace
+    connector, s3_parameters, service, arguments, hyperspace = src.preface.interface.Interface().exc()
+
+    main()
