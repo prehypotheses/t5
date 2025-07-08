520cd8df05f83de251adee0840f06051facad3ab
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-08 22:08:39 +0100

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -3,20 +3,25 @@ import logging
 
 import src.data.interface
 import src.elements.master as mr
+import src.elements.arguments as ag
+import src.elements.hyperspace as hp
 import src.elements.s3_parameters as s3p
 import ray
 
 
 class Interface:
 
-    def __init__(self, s3_parameters: s3p.S3Parameters):
+    def __init__(self, s3_parameters: s3p.S3Parameters, arguments: ag.Arguments, hyperspace: hp.Hyperspace):
         """
 
         :param s3_parameters:
+        :param arguments:
+        :param hyperspace:
         """
 
-        self.__master: mr.Master = src.data.interface.Interface(
-            s3_parameters=s3_parameters).exc()
+        self.__s3_parameters = s3_parameters
+        self.__arguments = arguments
+        self.__hyperspace = hyperspace
 
     def exc(self):
         """
@@ -24,10 +29,11 @@ class Interface:
         :return:
         """
 
-        logging.info(self.__master.id2label)
-        logging.info(self.__master.label2id)
-        logging.info(self.__master.data)
-        logging.info(self.__master.data['train'].features)
+        master: mr.Master = src.data.interface.Interface(
+            s3_parameters=self.__s3_parameters).exc()
 
-        train = ray.data.from_huggingface(self.__master.data['train'])
-        logging.info(train)
+        train = ray.data.from_huggingface(master.data['train'])
+        validation = ray.data.from_huggingface(master.data['validation'])
+
+
+
