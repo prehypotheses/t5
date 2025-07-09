80be2b02d73e2c58bbef961e7934923535d30563
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-10 00:07:52 +0100

    Update src/modelling/tuning.py

--- src/modelling/tuning.py
+++ src/modelling/tuning.py
@@ -1,9 +1,10 @@
 """Module tuning.py"""
 import logging
+
 import ray
 import ray.tune
 import ray.tune.schedulers as rts
-import ray.tune.search.optuna as opt
+import ray.tune.search.optuna as pta
 
 import src.elements.arguments as ag
 import src.elements.hyperspace as hp
@@ -24,9 +25,14 @@ class Tuning:
         self.__arguments = arguments
         self.__hyperspace = hyperspace
 
-        self.__space = {"per_device_train_batch_size": self.__arguments.TRAIN_BATCH_SIZE,
-                        "per_device_eval_batch_size": self.__arguments.VALID_BATCH_SIZE,
-                        "num_train_epochs": ray.tune.choice([5, 7, 9])}
+        self.__space = {
+            'learning_rate': ray.tune.uniform(
+                lower=min(self.__hyperspace.learning_rate_distribution),
+                upper=max(self.__hyperspace.learning_rate_distribution)),
+            'weight_decay': ray.tune.uniform(
+                lower=min(self.__hyperspace.weight_decay_distribution),
+                upper=max(self.__hyperspace.weight_decay_distribution)),
+            'per_device_train_batch_size': ray.tune.choice(self.__hyperspace.per_device_train_batch_size)}
 
     @staticmethod
     def compute_objective(metric):
@@ -49,36 +55,35 @@ class Tuning:
 
         return self.__space
 
-    def scheduler(self):
+    @staticmethod
+    def scheduler() ->  rts.AsyncHyperBandScheduler:
         """
         https://docs.ray.io/en/latest/tune/api/doc/ray.tune.schedulers.PopulationBasedTraining.html<br>
         https://docs.ray.io/en/latest/tune/api/doc/ray.tune.schedulers.AsyncHyperBandScheduler.html
-
-        :return:
-        """
 
         return rts.PopulationBasedTraining(
             time_attr='training_iteration',
             metric='eval_loss', mode='min',
             perturbation_interval=self.__arguments.perturbation_interval,
-            hyperparam_mutations={
-                'learning_rate': ray.tune.uniform(lower=min(self.__hyperspace.learning_rate_distribution),
-                                                  upper=max(self.__hyperspace.learning_rate_distribution)),
-                'weight_decay': ray.tune.uniform(lower=min(self.__hyperspace.weight_decay_distribution),
-                                                 upper=max(self.__hyperspace.weight_decay_distribution)),
-                'per_device_train_batch_size': self.__hyperspace.per_device_train_batch_size
-            },
+            hyperparam_mutations=self.__space,
             quantile_fraction=self.__arguments.quantile_fraction,
             resample_probability=self.__arguments.resample_probability)
-
-    @staticmethod
-    def algorithm():
-        """
 
         :return:
         """
 
-        return opt.OptunaSearch(metric='eval_loss', mode='min')
+        return rts.ASHAScheduler(
+            time_attr='training_iteration', metric='eval_loss', mode='min')
+
+    @staticmethod
+    def algorithm() -> pta.OptunaSearch:
+        """
+        ray.tune.search.optuna.OptunaSearch(metric='eval_loss', mode='min')
+
+        :return:
+        """
+
+        return pta.OptunaSearch(metric='eval_loss', mode='min')
 
     @staticmethod
     def reporting():
