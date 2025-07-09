0f8985c9f5b688d711ee408a2a699521f9d7a3db
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-09 14:26:54 +0100

    Update src/modelling/check.py

--- src/modelling/check.py
+++ src/modelling/check.py
@@ -8,12 +8,13 @@ class Check:
         self.__num_to_keep = 5
         self.__checkpoint_score_attribute = 'training_iteration'
 
-    def __call__(self):
+    def __call__(self) -> ray.tune.CheckpointConfig:
         """
         https://docs.ray.io/en/latest/tune/api/doc/ray.tune.CheckpointConfig.html
 
         :return:
         """
 
-        ray.tune.CheckpointConfig(num_to_keep=self.__num_to_keep,
-                                  checkpoint_score_attribute=self.__checkpoint_score_attribute)
+        return ray.tune.CheckpointConfig(
+            num_to_keep=self.__num_to_keep,
+            checkpoint_score_attribute=self.__checkpoint_score_attribute)
