345b05970af21835fb042d96020c105754014752
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-09 14:56:15 +0100

    Update src/modelling/interface.py

--- src/modelling/interface.py
+++ src/modelling/interface.py
@@ -82,7 +82,8 @@ class Interface:
             n_trials=self.__arguments.N_TRIALS, direction='minimize', backend='ray',
             resources_per_trial={'cpu': self.__arguments.N_CPU, 'gpu': self.__arguments.N_GPU},
             storage_path=os.path.join(self.__arguments.model_output_directory, 'ray'),
-            scheduler=tuning.scheduler(), checkpoint_config=checkpoint_config, verbose=0)
+            scheduler=tuning.scheduler(), checkpoint_config=checkpoint_config, verbose=0,
+            progress_reporter=tuning.reporting)
 
 
 
