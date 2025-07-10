b90fa6c1092660ba3e16a2986cb14ea5aec3de48
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-10 21:09:25 +0100

    Update src/modelling/steps.py: In Progress

--- src/modelling/steps.py
+++ src/modelling/steps.py
@@ -1,7 +1,41 @@
+import ray.train.torch
+import ray.train
 
+import src.modelling.check
 import src.modelling.intelligence
+import src.elements.arguments as ag
+import src.elements.hyperspace as hp
+import src.elements.s3_parameters as s3p
 
 class Steps:
 
-    def __init__(self):
-        pass
+    def __init__(self, s3_parameters: s3p.S3Parameters, arguments: ag.Arguments, hyperspace: hp.Hyperspace):
+        """
+
+        :param s3_parameters:
+        :param arguments:
+        :param hyperspace:
+        """
+
+        self.__s3_parameters = s3_parameters
+        self.__arguments = arguments
+        self.__hyperspace = hyperspace
+
+    def exc(self):
+        """
+        {'cpu': self.__arguments.N_CPU, 'gpu': self.__arguments.N_GPU}
+        :return:
+        """
+
+        intelligence = src.modelling.intelligence.Intelligence(
+            s3_parameters=self.__s3_parameters, arguments=self.__arguments, hyperspace=self.__hyperspace)
+
+        checkpoint_config = src.modelling.check.Check().__call__()
+
+        trainer = ray.train.torch.TorchTrainer(
+            intelligence.train_func,
+            scaling_config=ray.train.ScalingConfig(
+                resources_per_worker={'CPU': self.__arguments.N_CPU, 'GPU': self.__arguments.N_GPU},
+                use_gpu=True, num_workers=self.__arguments.N_GPU),
+            run_config=ray.train.RunConfig(checkpoint_config=checkpoint_config)
+        )
