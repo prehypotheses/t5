045c1e29135f4adf97473e427ae6c047c85289bb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-11 22:19:29 +0100

    Update src/master.py

---
+++ src/master.py
@@ -0,0 +1,73 @@
+"""Module script.py"""
+import datetime
+import logging
+import os
+import sys
+
+import boto3
+import ray
+import torch
+
+
+# noinspection DuplicatedCode
+def main():
+    """
+
+    :return:
+    """
+
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
+
+    # Device Selection: Setting a graphics processing unit as the default device
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    logger.info('device: %s', device)
+
+    # Ray
+    ray.init(dashboard_host='172.17.0.2', dashboard_port=8265)
+
+    # Best
+    best = src.modelling.layer.Layer(s3_parameters=s3_parameters, arguments=arguments, hyperspace=hyperspace).exc()
+    logger.info(best)
+    logger.info(best.hyperparameters)
+    logger.info(best.run_summary)
+    logger.info(best.__dir__())
+
+    # Cache
+    src.functions.cache.Cache().exc()
+
+# noinspection DuplicatedCode
+if __name__ == '__main__':
+
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    # Logging
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d\n',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # Activate graphics processing units
+    os.environ['CUDA_VISIBLE_DEVICES']='0'
+    os.environ['TOKENIZERS_PARALLELISM']='true'
+    os.environ['RAY_USAGE_STATS_ENABLED']='0'
+    os.environ['HF_HOME']='/tmp'
+
+    # Classes
+    import src.elements.arguments as ag
+    import src.elements.hyperspace as hp
+    import src.elements.s3_parameters as s3p
+    import src.elements.service as sr
+    import src.functions.cache
+    import src.modelling.layer
+    import src.preface.interface
+
+    connector: boto3.session.Session
+    s3_parameters: s3p.S3Parameters
+    service: sr.Service
+    arguments: ag.Arguments
+    hyperspace: hp.Hyperspace
+    connector, s3_parameters, service, arguments, hyperspace = src.preface.interface.Interface().exc()
+
+    main()
