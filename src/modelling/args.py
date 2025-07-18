ed79b1be2147901b30a5bcc0e532a4ba96b25ffb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-18 17:32:23 +0100

    Update src/modelling/args.py

--- src/modelling/args.py
+++ src/modelling/args.py
@@ -21,7 +21,7 @@ class Args:
         self.__arguments = arguments
         self.__n_instances = n_instances
 
-    def __call__(self) -> transformers.TrainingArguments:
+    def __call__(self, branch: str) -> transformers.TrainingArguments:
         """
 
         :return:
@@ -35,7 +35,7 @@ class Args:
                 max_steps = -1
 
         args = transformers.TrainingArguments(
-            output_dir=self.__arguments.model_output_directory,
+            output_dir=os.path.join(self.__arguments.model_output_directory, branch),
             report_to='tensorboard',
             eval_strategy='epoch',
             save_strategy='epoch',
@@ -53,7 +53,7 @@ class Args:
             metric_for_best_model='eval_loss',
             greater_is_better=False,
             load_best_model_at_end=True,
-            logging_dir=os.path.join(self.__arguments.model_output_directory, 'logs'),
+            logging_dir=os.path.join(self.__arguments.model_output_directory, branch, 'logs'),
             fp16=True,
             push_to_hub=False)
 
