b84ed190624b85c04cb3ce086856fdd10a4cb4d8
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-28 23:29:44 +0100

    Update config.py

--- config.py
+++ config.py
@@ -19,19 +19,33 @@ class Config:
         excluded for names such as warehouse, storage, depository, etc.<br><br>
         """
 
-        # Directories
+        '''
+        Experiment
+        '''
+        self.experiment_name = 'FEW'
+        self.experiment_tags = {
+            'project': 'custom token classification',
+            'type': 'natural language processing',
+            'task': 'token classification',
+            'description': 'The fine-tuning of pre-trained large language model architectures for token classification tasks.'}
+
+        '''
+        Directories
+        '''
         self.data_ = os.path.join(os.getcwd(), 'data')
         self.warehouse = os.path.join(os.getcwd(), 'warehouse')
         self.artefacts_ = os.path.join(self.warehouse, 'artefacts')
 
-        # Keys, etc
+        '''
+        Keys
+        '''
         self.s3_parameters_key = 's3_parameters.yaml'
         self.arguments_key = 'architecture/t5/arguments.json'
         self.hyperspace_key = 'architecture/t5/hyperspace.json'
 
         '''
-       The metadata of the modelling artefacts
-       '''
+        The metadata of the modelling artefacts
+        '''
         self.metadata = {'description': 'The modelling artefacts of {architecture}.',
                          'details': 'The {architecture} collection consists of (a) the checkpoints, (b) the logs ' +
                                     'for TensorBoard examination, and (c) much more.'}
