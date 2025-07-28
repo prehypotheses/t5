07dfc18d4bc43d54a5203866ec2bc02900b7f709
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-28 22:25:33 +0100

    Update src/valuate/interface.py: In Progress

--- src/valuate/interface.py
+++ src/valuate/interface.py
@@ -1,10 +1,13 @@
 """Module interface.py"""
+import os
+
 import datasets
 import transformers
 
+import src.elements.arguments as ag
+import src.functions.secret
 import src.valuate.estimates
 import src.valuate.measurements
-
 
 
 class Interface:
@@ -12,15 +15,17 @@ class Interface:
     Interface
     """
 
-    def __init__(self, model: transformers.Trainer, id2label: dict):
+    def __init__(self, model: transformers.Trainer, id2label: dict, arguments: ag.Arguments):
         """
 
         :param model:
         :param id2label:
+        :param arguments:
         """
 
         self.__model = model
         self.__id2label = id2label
+        self.__arguments = arguments
 
         self.__experiment_name = 'FEW'
         self.__experiment_tags = {
@@ -28,13 +33,21 @@ class Interface:
             'task': 'token classification', 'team': 'data science core',
             'description': 'Token classification via the fine tuning of pre-trained large language model architectures'}
 
-    def exc(self, blob: datasets.Dataset, path: str):
+    def __parts(self):
+
+        pass
+
+
+    def exc(self, blob: datasets.Dataset, branch: str, stage: str):
         """
 
         :param blob:
-        :param path:
+        :param branch:
+        :param stage:
         :return:
         """
+
+        path = os.path.join(self.__arguments.model_output_directory, branch, 'metrics', stage)
 
         originals, predictions = src.valuate.estimates.Estimates(
             blob=blob, id2label=self.__id2label).exc(model=self.__model)
