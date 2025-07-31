001e661e55ad33cfc6cfb2eff31e6f77df4f6bd4
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-31 21:22:27 +0100

    Update src/preface/arguments.py

--- src/preface/arguments.py
+++ src/preface/arguments.py
@@ -1,5 +1,46 @@
+import os
+import datetime
+import time
+
+import boto3
+
+import config
+import src.s3.configurations
+import src.elements.arguments as ag
+
 
 class Arguments:
 
     def __init__(self):
-        pass
+
+        self.__configurations = config.Config()
+
+        today = datetime.datetime.now().strftime('%Y-%m-%d')
+        pattern = datetime.datetime.strptime(today, '%Y-%m-%d %H:%M:%S')
+        self.__seconds = int(time.mktime(pattern.timetuple()))
+
+    def __get_arguments(self, connector: boto3.session.Session) -> ag.Arguments:
+        """
+
+        :param connector:
+        :return:
+        """
+
+        dictionary = src.s3.configurations.Configurations(connector=connector).objects(
+            key_name=self.__configurations.arguments_key)
+
+        # Set up the model output directory parameter
+        dictionary['experiment_segment'] = str(self.__seconds)
+        dictionary['model_output_directory'] = os.path.join(
+            self.__configurations.artefacts_, dictionary['architecture'].upper(), dictionary['experiment_segment'])
+
+        return ag.Arguments(**dictionary)
+
+    def __call__(self, connector: boto3.session.Session):
+        """
+
+        :param connector:
+        :return:
+        """
+
+        return self.__get_arguments(connector=connector)
