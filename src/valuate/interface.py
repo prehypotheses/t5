4879e67df51c5cbbf0567bf08bbf7860b7cf5ff0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-31 13:16:17 +0100

    Update src/valuate/interface.py

--- src/valuate/interface.py
+++ src/valuate/interface.py
@@ -1,14 +1,10 @@
 """Module interface.py"""
 import os
 
-import boto3
 import datasets
-import mlflow
 import transformers
 
-import config
 import src.elements.arguments as ag
-import src.functions.secret
 import src.valuate.estimates
 import src.valuate.measurements
 
@@ -18,11 +14,9 @@ class Interface:
     Interface
     """
 
-    def __init__(self, connector: boto3.session.Session, model: transformers.Trainer, id2label: dict, arguments: ag.Arguments):
+    def __init__(self, model: transformers.Trainer, id2label: dict, arguments: ag.Arguments):
         """
 
-        :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
-                          Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
         :param model:
         :param id2label:
         :param arguments:
@@ -31,42 +25,6 @@ class Interface:
         self.__model = model
         self.__id2label = id2label
         self.__arguments = arguments
-
-        # Instances
-        self.__configurations = config.Config()
-        self.__secret = src.functions.secret.Secret(connector=connector)
-
-        # Tracking Resource
-        self.__tracking_uri = self.__get_tracking_uri()
-
-    def __get_tracking_uri(self) -> str:
-        """
-
-        :return:
-        """
-
-        t_secret  = self.__secret.exc(secret_id='FNTC', node='tracking-secret')
-        t_endpoint = self.__secret.exc(secret_id='FNTC', node='tracking-endpoint')
-        t_database = self.__secret.exc(secret_id='FNTC', node='tracking-database')
-        t_port = self.__secret.exc(secret_id='FNTC', node='tracking-port')
-
-        username = self.__secret.exc(secret_id=t_secret, node='username')
-        password = self.__secret.exc(secret_id=t_secret, node='password')
-
-        uri: str = f"postgresql://{username}:{password}@{t_endpoint}:{t_port}/{t_database}"
-
-        return uri
-
-    def __experiment(self):
-        """
-        if it does not exist ... in progress
-
-        :return:
-        """
-
-        client = mlflow.MlflowClient()
-        client.create_experiment(name=self.__configurations.experiment_name,
-                                 tags=self.__configurations.experiment_tags)
 
     def exc(self, blob: datasets.Dataset, branch: str, stage: str):
         """
