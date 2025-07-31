e658ac7081abcef30b5ac9bc89af859d688d4c84
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-07-31 21:45:25 +0100

    Update src/elements/master.py

--- src/elements/master.py
+++ src/elements/master.py
@@ -7,6 +7,13 @@ import datasets
 class Master(typing.NamedTuple):
     """
     The data type class ⇾ Master
+
+    id2label: dict
+        A dictionary wherein (a) the keys are the identification codes of text labels, and (b) the values are the labels.
+    label2id: dict
+        A dictionary wherein (a) the values are the labels, and (b) the keys are the identification codes of text labels.
+    data: datasets: DatasetDict
+        The training, validation, and testing data; named `train`, `validation`, and `test`, respectively.
     """
 
     id2label: dict
